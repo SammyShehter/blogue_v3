21a3f281bbf1feb74736572ede988a6bf58268da
Sammy Shehter <samir_shehter@yahoo.com>
2021-08-20 09:20:21 +0300

    little chenge

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const  http = require('http')
 
 const indexRouter = require('./routes/index')
 
 const app = express()
+const server = http.createServer(app)
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -14,8 +16,12 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-const PORT = 9000
+const PORT = 6000
 
-console.log(`Server started at ${PORT}`)
+
+
+server.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`)
+})
 
 module.exports = app
